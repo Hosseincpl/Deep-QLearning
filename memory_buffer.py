67cae8d10b072f8cc64e0a103be0da4249cfc722
Hossein Karami <Hosseincpl@gmail.com>
2023-04-13 11:18:18 +0330

    Init memory buffer size module

---
+++ memory_buffer.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+class Memory():
+    def __init__(self, memory_size, batch_size):
+        self.buffer = deque(mxlen=memory_size)
+        self.batch_size = batch_size
+    
+    def add(self, exprience):
+        # adds a (state, action, reward, state_prime, done) tuple
+        self.buffer.append(exprience)
+    
+    def sample(self):
+        buffer_size = len(self.buffer)
+        index = np.random.choice(
+            np.arange(buffer_size), size=self.batch_size, replace=False
+        )
+        batch = [self.buffer[i] for i in index]
+        return batch
